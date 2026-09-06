f3ce7c2d01b5d0b6b6ca3951c2bf111ec7c046be
Chandan Gorain <chandangorain.cse2023@nsec.ac.in>
2026-09-07 00:40:15 +0530

    streaming issue solved

---
+++ streamlit_frontend_streaming.py
@@ -0,0 +1,47 @@
+# streaming means the model starts sending tokens as soon as the are generated , in stead
+#of waiting for the entire response to be generated. This is useful for large responses, as it allows the user to start reading the response before it is fully generated.
+
+# in normal : graph.invoke('')
+# in streaming : graph.stream('')
+
+import streamlit as st
+from langgraph_backend import chatbot
+from langchain_core.messages import HumanMessage
+
+# st.session_state -> dict -> 
+CONFIG = {'configurable': {'thread_id': 'thread-1'}}
+
+if 'message_history' not in st.session_state:
+    st.session_state['message_history'] = []
+
+# loading the conversation history
+for message in st.session_state['message_history']:
+    with st.chat_message(message['role']):
+        st.text(message['content'])
+
+#{'role': 'user', 'content': 'Hi'}
+#{'role': 'assistant', 'content': 'Hi=ello'}
+
+user_input = st.chat_input('Type here')
+
+if user_input:
+
+    # first add the message to message_history
+    st.session_state['message_history'].append({'role': 'user', 'content': user_input})
+    with st.chat_message('user'):
+        st.text(user_input)
+
+    # first add the message to message_history
+    with st.chat_message('assistant'):
+
+# we are getting message_chunk, metadata from chatbot.stream()
+#then we are sending content of the message_chunk to st.write_stream() which will display the content 
+        ai_message = st.write_stream(
+            message_chunk.content for message_chunk, metadata in chatbot.stream(
+                {'messages': [HumanMessage(content=user_input)]},
+                config= {'configurable': {'thread_id': 'thread-1'}},
+                stream_mode= 'messages'
+            )
+        )
+
+    st.session_state['message_history'].append({'role': 'assistant', 'content': ai_message})
