64aa8dd7779e91731f8bdd68f6b30218e0bbdfd9
Chandan Gorain <chandangorain.cse2023@nsec.ac.in>
2026-09-11 16:21:43 +0530

    tool integration

--- langgraph_tool_backend.py
+++ langgraph_tool_backend.py
@@ -101,7 +101,7 @@ graph.add_node("tools", tool_node)
 graph.add_edge(START, "chat_node")
 
 graph.add_conditional_edges("chat_node",tools_condition)
-graph.add_edge('tools', 'chat_node')
+graph.add_edge('tools', 'chat_node')   # tool <>chatnode
 
 chatbot = graph.compile(checkpointer=checkpointer)
 
